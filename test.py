2e77003f96e22e1d2b3e85ddc32304c5d8c0b4ff
ly940114 <100075linda@gmail.com>
2023-06-25 17:32:05 +0800

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+aaa = '1' #字串
+bbb = 1 #數字
+ccc = True #布林值
+ddd = False #布林值
+
+print(aaa)
+print(bbb)
+print(ccc)
+print(ddd)
+
+print(type(aaa))
+print(type(bbb))
+print(type(ccc))
+print(type(ddd))
---
+++ test2.py
@@ -0,0 +1,5 @@
+aaa = input("請輸入字串")
+bbb = input()
+
+print(aaa)
+print(bbb)
