f601b84d879531228663a0a0691180bdeb278e82
ly940114 <100075linda@gmail.com>
2023-06-25 23:21:41 +0800

    type

--- type.py
+++ type.py
@@ -3,12 +3,19 @@ bbb = 1 #數字
 ccc = True #布林值
 ddd = False #布林值
 
-print(aaa)
-print(bbb)
-print(ccc)
-print(ddd)
+#數字
+#整數(int)
+dd = 3
+#小數點(float)浮點數
+ee = 3.14
 
+#type()為查看資料型別
 print(type(aaa))
 print(type(bbb))
 print(type(ccc))
 print(type(ddd))
+print(type(dd))
+print(type(ee))
+
+print(5/2)
+print(int(5/2))
